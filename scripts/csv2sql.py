4b7476f83a97812c1383d88390b776b393f2d79e
Giovanni Sce <giosce@verizon.net>
2022-06-25 14:55:22 -0400

    Progressing in loading datasets.

--- scripts/csv2sql.py
+++ scripts/csv2sql.py
@@ -28,13 +28,10 @@ db_user = os.getenv('DB_USER')
 db_pwd = os.getenv('DB_PWD')
 db_name = os.getenv('DB_NAME')
 
-# it should first unzip the all-counties file
-# then unzip all county files
-# then process each county file like below
 
-filename = sys.argv[1] #"AZ_Chandler_calls_for_service_2021.csv"
+filename = sys.argv[1] #filename - make it to handle a directory too
 
-table_name = "911"
+table_name = "911" #move table_name in .env?
 
 # do more index like party, town, status
 #dropCountyIndexSql = "drop index county_idx on " + table_name
@@ -89,200 +86,12 @@ elif filename.find('ems') >= 0:
 inData['agency'] = agency
   
 
-# For Tucson
-if(filename == "AZ.Tuscon.2021.Calls.csv"):
-  inData.drop(inplace=True, columns=['OBJECTID', 'zip', 'ACTDATE', 'ACTTIME', 'ACT_MONTH', 'ACT_YEAR', 'ACT_DOW', 'ACT_TIME', 'LOC_STATUS', 'WARD', 
-  'DIVISION', 'DIVSECT', 'TRSQ', 'City_geo', 'ADDRESS_100BLK'])
-
-  inData.rename(
-    {
-    'X': 'incident_coord_x', 
-    'Y': 'incident_coord_y', 
-    'case_id': 'report_id',
-    'acci_id': 'incident_id',
-    'agency': 'entity_dispatched_type',
-    'ADDRESS_PUBLIC': 'incident_address', # concatenate multiple source fields?
-    'city': 'municipality',
-    'NEIGHBORHD': 'incident_location',
-    'emdivision': 'incident_location_2',
-    'ACTDATETIME': 'call_received_time',
-    'CAPRIORITY': 'call_priority',
-    'NATURECODE': 'call_type',
-    'NatureCodeDesc': 'call_description',
-    'HOWRECEIVE': 'call_method',
-    'CSDISPOSIT': 'disposition_code',
-    'DispositionCodeDesc': 'disposition_description',
-    'NHA_NAME': 'incident_location_3',
-    'DIVISION_NO': 'district'
-    }, 
-  axis=1, inplace=True)
-
-  inData['municipality'] = 'Tucson'
-  inData['county'] = 'Pima'
-  inData['state'] = 'AZ'
-
-elif(filename == "CA.SanDiego.2021.Calls.csv"):
-
-  inData["incident_address"] = inData['address_number_primary'].astype(str) + " " + inData['address_dir_primary'] \
-  + " " + inData['address_road_primary'] + " " + inData['address_sfx_primary']
-  
-  inData["incident_location"] = inData['address_dir_intersecting'] \
-  + " " + inData['address_road_intersecting'] + " " + inData['address_sfx_intersecting']
-
-  inData.drop(inplace=True, columns=['day_of_week', 'address_number_primary', 'address_dir_primary', 'address_road_primary',
-  'address_sfx_primary', 'address_dir_intersecting', 'address_road_intersecting', 'address_sfx_intersecting'])
-
-  inData.rename(
-    {
-    'incident_num': 'incident_id',
-    'date_time': 'call_received_time',
-    'disposition': 'disposition_code',
-    'priority': 'call_priority'
-    }, 
-  axis=1, inplace=True)
-
-  inData['municipality'] = 'San Diego'
-  inData['county'] = 'San Diego'
-  inData['state'] = 'CA'
-
-elif(filename == "CA.Sacramento.2019.Calls.csv"):
-
-  inData.rename(
-    {
-    'X': 'incident_coord_x', 
-    'Y': 'incident_coord_y',
-    'Record_ID': 'original_record_id',
-    'Call_Type': 'call_type',
-    'Description': 'call_description',
-    'Reporting_Officer': 'call_taker',
-    'Unit_ID': 'unit_dispatched_id',
-    'Report_Created': 'report_created',
-    'Location': 'incident_address',
-    'Police_District': 'district',
-    'Occurence_Date': 'incident_datetime',
-    'Received_Date': 'call_received_time',
-    'Dispatch_Date': 'dispatch_datetime',
-    'Enroute_Date': 'enroute_datetime',
-    'At_Scene_Date': 'at_scene_datetime',
-    'Clear_Date': 'clear_datetime'
-    }, 
-  axis=1, inplace=True)
-
-  inData.drop(inplace=True, columns=['OBJECTID', 'Grid', 'X_Coordinate', 'Y_Coordinate', 'Day_of_Week'])
-
-  inData['municipality'] = 'Sacramento'
-  inData['county'] = 'Pima'
-  inData['state'] = 'CA'
-
-elif(filename == "MD.Baltimore.2021.Calls.csv"): # 2020 and older have 1.4 million records
-
-  inData.rename(
-    {
-    'callDateTime': 'call_received_time', 
-    'priority': 'call_priority',
-    'description': 'call_description',
-    'callNumber': 'call_id', 
-    'location': 'incident_address', 
-    'Neighborhood': 'incident_location', 
-    'PoliceDistrict': 'district', 
-    'SheriffDistricts': 'district_2', 
-    'Community_Statistical_Areas': 'incident_location_2'
-    }, 
-  axis=1, inplace=True)
-
-  inData.drop(inplace=True, columns=['OBJECTID', 'callKey', 'incidentLocation', 'PolicePost', 'CouncilDistrict', 
-  'Census_Tracts', 'VRIZones', 'ZIPCode', 'NeedsSync', 'IsDeleted'])
-
-  inData['municipality'] = 'Baltimore'
-  #inData['county'] = '' Wikipedia says is independent, not in a county
-  inData['state'] = 'MD'
-
-elif(filename == "AZ.Mesa.Calls.csv"):
-
-  inData.rename(
-    {
-    'Event Number': 'call_id', 
-    'Creation Datetime': 'call_received_time',
-    'Event Type Code': 'call_type',
-    'Event Type Description': 'call_description',
-    'Call Priority': 'call_priority',
-    'Event Address': 'incident_address',
-    'Longitude': 'incident_coord_x',
-    'Latitude': 'incident_coord_y',
-    'Dispatch Group': 'unit_dispatched_id',
-    'Division': 'incident_location',
-    'Response Time Seconds': 'response_time'
-    }, 
-  axis=1, inplace=True)
-
-  inData.drop(inplace=True, columns=['Creation Month', 'Creation Year', 'Street Number', 'Street Direction', 'Street Name', 
-  'Street Type', 'Response Time Minutes', 'Location 1'])
-  
-  inData['municipality'] = 'Mesa'
-  inData['county'] = 'Maricopa'
-  inData['state'] = 'AZ'
-
-elif(filename == "CT.Hartford.Calls.csv"):
-
-  inData.rename(
-    {
-    'APNO': 'call_id',   
-    'Address': 'incident_address',
-    'Neighborhood': 'incident_location',
-    'Priority': 'call_priority',
-    'Call_Initiated_By': 'call_initiated_by'
-    }, 
-  axis=1, inplace=True)
-
-  inData['call_received_time'] = inData['Date'].astype(str) + " " + inData['Time_24HR'].astype(str) #need to put a : in between time number
-  inData['call_description'] = inData['Description_1'] + " " + inData['Description_2']
-  inData["geom"].replace("(),","").str.split(' ', expand=True)
-  #x_y = geom.str.split(' ', expand=True)
-  #inData['incident_coord_x'] = x_y[0]
-  #inData['incident_coord_y'] = x_y[1]
-
-  inData.drop(inplace=True, columns=['Date', 'Time_24HR', 'Description_1', 'Description_2']) #, 'geom'])
-  
-  inData['municipality'] = 'Hartford'
-  inData['county'] = ''
-  inData['state'] = 'CT'
-
-elif(filename == "NYPD_Calls_for_Service__Year_to_Date_.csv"):
-
-  inData.rename(
-    {
-    'CAD_EVNT_ID': 'call_id',
-    'CREATE_DATE': 'call_date',
-    'NYPD_PCT_CD': 'district',
-    'BORO_NM': 'incident_location',
-    'PATRL_BORO_NM': 'beat',
-    'RADIO_CODE': 'call_type',
-    'TYP_DESC': 'call_description',
-    'ADD_TS': 'call_received_time',
-    'DISP_TS': 'dispatch_datetime',
-    'ARRIVD_TS': 'at_scene_datetime',
-    'CLOSNG_TS': 'clear_datetime',
-    'Latitude': 'incident_coord_x',
-    'Longitude': 'incident_coord_y'
-    }, 
-  axis=1, inplace=True)
-
-  inData['incident_datetime'] = inData['INCIDENT_DATE'].astype(str) + " " + inData['INCIDENT_TIME'].astype(str) #need to put a : in between time number
-  
-  inData.drop(inplace=True, columns=['INCIDENT_DATE', 'INCIDENT_TIME', 'GEO_CD_X', 'GEO_CD_Y'])
-  
-  inData['municipality'] = 'New York'
-  inData['county'] = ''
-  inData['state'] = 'NY'
-
-  print(inData['call_date'].max())
-  print(inData['call_date'].min())
-
-  # this file has more than 6M records, from 1/1/21 to 12/31/21
-
 # processing 55 datasets from R911 Inventory of Datasets in June 2022
 
-elif(filename == "AZ_Chandler_calls_for_service_2021.csv"):
+# make pandas changes in function? Not a big improvement as changes are different for each file
+# common code is outside of the if / elif statements
+
+if(filename == "AZ_Chandler_calls_for_service_2021.csv"):
 
   inData.rename(
     {
@@ -539,14 +348,84 @@ elif(filename == "CA_SanJose_policecalls2021.csv"):
   inData.drop(inplace=True, columns=['CDTS','EID','START_DATE','REPORT_DATE','OFFENSE_DATE','OFFENSE_TIME',
   'FINAL_DISPO_CODE','ADDRESS','STATE'])
   
+elif(filename == "CA_SanJose_sanjosefireincidentdata_2021.csv"):
+
+  inData.rename(
+    {
+    'Incident_No': 'id_in_dataset',
+	'Unit_On_Scene_TimeStamp': 'on_scene_datetime',
+	'Cleared_TimeStamp': 'closed_datetime',
+	'Final_Incident_Type': 'event_type_final',
+	'Priority': 'call_priority',
+	'Final_Incident_Category': 'event_category',
+	'Station': 'agency_precinct'
+    }, 
+  axis=1, inplace=True)
+  
+  inData['call_datetime'] = pd.to_datetime(inData['Date_Time_Of_Event']) 
+
+  inData.drop(inplace=True, columns=['Date_Time_Of_Event','Dispatched_Time','Unit_On_The_Way_Time',
+  'On_Scene_Unit','Unit_Count','Near_X','Near_Y','Battalion'])
+  
+elif(filename == "FL_Gainesville_Crime_Responses.csv"):
+# these perhaps are not calls at all
+
+  inData.rename(
+    {
+    'ID': 'id_in_dataset',
+	'Offense Date': 'call_datetime',
+	# there is a report_date in file but it is later than offense_date
+	'Incident Type': 'event_type',
+	'City': 'incident_city'
+    }, 
+  axis=1, inplace=True)
+  
+  #inData['call_datetime'] = pd.to_datetime(inData['Date_Time_Of_Event']) 
+
+  inData.drop(inplace=True, columns=['Report Date','Report Hour of Day','Report Day of Week',
+  'Offense Hour of Day','Offense Day of Week','State','Address','Latitude','Longitude','Location'])
+  
+elif(filename == "FL_Gainesville_Fire_Rescue_Responses.csv"):
+
+  inData.rename(
+    {
+    'Master Incident Number': 'id_in_dataset',
+	'Response Type': 'agency',
+	'Location Name': 'incident_location'
+    }, 
+  axis=1, inplace=True)
+
+  inData['call_datetime'] = pd.to_datetime(inData['Response Date']) 
+  
+  inData.drop(inplace=True, columns=['Response Hour of Day','Response Day of Week','Responding Unit',
+  'Longitude','Latitude','Location','Response Date'])
+
+elif(filename == "FL_StPete_Police_Calls.csv"):
+
+  inData.rename(
+    {
+    'ID': 'id_in_dataset',
+	'Type of Event': 'event_type',
+	'Event Subtype': 'event_category', # questionable
+	'Classification': 'disposition', # questionable
+	'Neighborhood Name': 'incident_city_neighborhood'
+    }, 
+  axis=1, inplace=True)
+
+  inData['call_datetime'] = pd.to_datetime(inData['Crime date']) 
+  
+  inData.drop(inplace=True, columns=['Event Number','Event Case Number','Display Address','Crime date','Crime Time',
+  'Longitude','Latitude','Location','Submit an anonymous tip','Council District','Event Subtype - Type of Event'])
+
 else:
   print("Not ready to process", filename)
   exit()
 
-pd.reset_option('max_columns')
-#pd.set_option('max_columns', None)
+
+#pd.reset_option('max_columns')
+#pd.set_option('max_columns', None) # doesn't work
 print(inData.head(10))
-#print(inData[['0', '1', '2','3','4','5','6']].head(10))
+#print(inData[['0', '1']].head(10)) doesn't work
 
 a = filename.split("_")
 inData['state'] = a[0]
@@ -567,6 +446,7 @@ time_2 = datetime.datetime.now()
 
 row_inserted = inData.to_sql(table_name, con = engine, if_exists = 'append', chunksize = 50000, index=False)
 # method='multi' is slower
+# it doesn't return the number of rows inserted
 
 #print("Data loaded in DB", row_inserted, str(datetime.datetime.now().time()))
 tdelta = datetime.datetime.now() - time_2
